0360843f8f3ba5b07841e1debed2ef72ecdeeb34
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2024-05-13 06:30:01 -0600

    Initial commit.

---
+++ hers_diagnostic_output/comparison.py
@@ -0,0 +1,8 @@
+from hers_diagnostic_output import HERSDiagnosticData
+
+
+class Comparison:
+
+    def __init__(self, data_objects: list[HERSDiagnosticData]):
+
+        self.data_objects = data_objects
