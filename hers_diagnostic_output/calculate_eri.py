d7f00b9a4f24533888984a02e266b678c30baa55
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2024-11-07 09:56:52 -0700

    Call all files in example directory.

--- hers_diagnostic_output/calculate_eri.py
+++ hers_diagnostic_output/calculate_eri.py
@@ -1,9 +1,23 @@
-from hers_diagnostic_output import HERSDiagnosticData
+from .hers_diagnostic_output import HERSDiagnosticData
+from pathlib import Path
 
-file_path = "examples/baltimore-multiclimate-base-case.json"
-data = HERSDiagnosticData(file_path)
-data.verify()
-eri = data.calculate_hers_index()
-co2 = data.calculate_carbon_index()
-print(f"HERS Index: {eri:.2f}")
-print(f"CO2 Index:  {co2:.2f}")
+directory_path = Path("examples")
+file_paths = [file for file in directory_path.iterdir()]  # add if statement
+
+energy_systems = ["space_heating", "space_cooling", "water_heating"]
+other_end_uses = ["lighting_and_appliance", "ventilation", "dehumidification"]
+
+
+for file_path in file_paths:
+    print(f"""\n{file_path.name.replace(".json", "")}""")
+    data = HERSDiagnosticData(file_path)
+    rated_home_data = data.data["rated_home_output"]
+    systems = [
+        system
+        for system in list(rated_home_data.keys())
+        if system != "conditioned_space_temperature"
+    ]
+
+    data.verify()
+    eri = data.calculate_hers_index()
+    co2 = data.calculate_carbon_index()
