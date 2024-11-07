0ca796a768987fa6b1d6a36c43290c2bd168e954
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2024-11-07 09:58:17 -0700

    Add task to calculate HERS Index.

--- dodo.py
+++ dodo.py
@@ -5,8 +5,9 @@ doit task/build automation
 import os
 
 from lattice import Lattice
+from hers_diagnostic_output.hers_diagnostic_output import HERSDiagnosticData
 
-data_model = Lattice()
+data_model = Lattice(build_validation=False)
 
 
 def task_generate_web_docs():
@@ -17,3 +18,9 @@ def task_generate_web_docs():
         "targets": [os.path.join(data_model.web_docs_directory_path, "public")],
         "actions": [(data_model.generate_web_documentation, [])],
     }
+
+
+def task_calculate_hers_index():
+    """Calculates HERS Index"""
+    for example_file in data_model.examples:
+        print(HERSDiagnosticData(example_file).calculate_hers_index())
