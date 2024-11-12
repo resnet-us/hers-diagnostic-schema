1f1595665212223865e75cd68fff0514471693c0
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2024-11-12 15:29:25 -0700

    Change example files' directory.

--- scripts/calculate_eri.py
+++ scripts/calculate_eri.py
@@ -3,14 +3,8 @@
 from pathlib import Path
 from hers_diagnostic_output.hers_diagnostic_output import HERSDiagnosticData
 
-directory_path = Path("examples", "input_files")
-# file_paths = [file for file in directory_path.glob("*.json")]
-file_paths = [
-    "examples/base.json"
-    # "examples/base-pv.json",
-    # "examples/base-pv-battery.json"
-]
-
+directory_path = Path("examples")
+file_paths = [file for file in directory_path.glob("*.json")]
 
 for file_path in file_paths:
     data = HERSDiagnosticData(file_path)
